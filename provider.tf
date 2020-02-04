cdf0843a4010dd6a0d2bc7bcb02b96f2777ce972
Tim Gatzemeier <tim.gatzemeier@gmx.de>
2020-02-04 18:15:19 +0100

    configure github provider with is_individual

--- provider.tf
+++ provider.tf
@@ -2,9 +2,14 @@ terraform {
   required_version = ">= 0.12.1"
 
   required_providers {
-    github = "~> 2.2"
     random = "~> 2.2"
     null   = "~> 2.1"
     local  = "~> 1.3"
   }
+}
+
+provider "github" {
+  version      = "~> 2.2"
+  individual   = var.is_individual
+  organization = var.github_organization
 }

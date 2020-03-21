cc246834bdfdd3105a8a8a1768a960c8fe0f25a1
etwillbefine <tim.gatzemeier@gmx.de>
2020-03-21 20:39:48 +0100

    wait for additional files sync before applying atlantis

--- atlantis.tf
+++ atlantis.tf
@@ -16,7 +16,7 @@ locals {
 
 module "initial_atlantis_commit" {
   source             = "git::https://github.com/goci-io/terraform-git-commit.git?ref=tags/0.2.0"
-  commit_depends_on  = [module.initial_actions_commit]
+  commit_depends_on  = [module.sync_additional_commit]
   git_repository     = local.repository_name
   git_organization   = var.github_organization
   git_base_url       = var.github_base_url

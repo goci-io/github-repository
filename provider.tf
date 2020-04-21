01ffccdc0970003909abddd828407fff6e8dc30a
etwillbefine <tim.gatzemeier@gmx.de>
2020-04-21 23:10:12 +0200

    set fixed version for template provider

--- provider.tf
+++ provider.tf
@@ -2,10 +2,11 @@ terraform {
   required_version = ">= 0.12.1"
 
   required_providers {
-    random = "~> 2.2"
-    null   = "~> 2.1"
-    local  = "~> 1.3"
-    tls    = "~> 2.1"
+    template = "~> 2.1"
+    random   = "~> 2.2"
+    null     = "~> 2.1"
+    local    = "~> 1.3"
+    tls      = "~> 2.1"
   }
 }
 

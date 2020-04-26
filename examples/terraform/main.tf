96e690024d63fc9689c251504f6e84d5526365f3
etwillbefine <tim.gatzemeier@gmx.de>
2020-04-26 12:54:59 +0200

    add example repository for github action

---
+++ examples/terraform/main.tf
@@ -0,0 +1,26 @@
+variable "github_token" {}
+
+module "tf_example_repo" {
+  source                        = "../../"
+  homepage_url                  = "https://github.com/goci-io/github-repository"
+  repository_name               = "goci-repository-setup-example"
+  repository_description        = "Example setup of an repository with Terraform workflow using Github Actions"
+  gitignore_template            = "Terraform"
+  github_organization           = "goci-io"
+  github_token                  = var.github_token
+  repository_visibility_private = false
+  create_repository             = false # Manually created to remove need of state file
+  repository_checkout_dir       = abspath("${path.module}/checkout")
+  status_checks                 = ["Terraform Format", "Terraform Plan"]
+  topics                        = ["terraform", "github", "example"]
+  actions = {
+    "terraform/format" = {}
+    "terraform/plan" = {
+      environment = {}
+    }
+    "terraform/apply" = {
+      name        = "Terraform Example"
+      environment = {}
+    }
+  }
+}
